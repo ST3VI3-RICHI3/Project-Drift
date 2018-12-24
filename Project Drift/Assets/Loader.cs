1a32175dc884c5119cb0adf508c552d8cdbae658
ST3VI3 RICHI3 <30847371+ST3VI3-RICHI3@users.noreply.github.com>
2018-12-24 18:15:07 +0000

    Added load script, Updated start scene

---
+++ Project Drift/Assets/Loader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Loader : MonoBehaviour
+{
+    public Slider slider;
+
+    public void LoadLevel(int SceneIndex)
+    {
+        StartCoroutine(LoadAsync(SceneIndex));
+    }
+
+    IEnumerator LoadAsync(int SceneIndex)
+    {
+        AsyncOperation Load = SceneManager.LoadSceneAsync(SceneIndex);
+        while (!Load.isDone)
+        {
+            slider.value = Load.progress;
+            yield return null;
+        }
+    }
+}

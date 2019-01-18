0aefb54569ccd98229ece4582cf796e4f71753e3
ST3VI3 RICHI3 <30847371+ST3VI3-RICHI3@users.noreply.github.com>
2019-01-18 19:19:50 +0000

    Added a console, updated menu, started to work on showcase levels should the need arise.

---
+++ Project Drift/Assets/Script/ConsoleHandle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ConsoleHandle : MonoBehaviour {
+
+    // Update is called once per frame
+    public InputField CommandLineFeild;
+	public void CommandSend() {
+        string CMDBuffer = null;
+        CMDBuffer = CommandLineFeild.text;
+        CommandLineFeild.text = "";
+        if (CMDBuffer.Length >= 9)
+        {
+            if (CMDBuffer.Substring(0, 9) == "LoadScene")
+            {
+                if (CMDBuffer.Length >= 11)
+                {
+                    try
+                    {
+                        SceneManager.LoadScene(int.Parse(CMDBuffer.Substring(10)));
+                    }
+                    catch
+                    {
+                        SceneManager.LoadScene(CMDBuffer.Substring(10));
+                    }
+                }
+                else
+                {
+                    //invalidSyntaxErr;
+                }
+            }
+            
+        }
+        else
+        {
+            //UnknownCMDErr;
+        }
+	}
+}
---
+++ Project Drift/Assets/Script/ConsoleListen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsoleListen : MonoBehaviour {
+
+    public KeyCode Key;
+    public GameObject ConsoleHandler;
+    public GameObject ConsoleObject;
+	// Use this for initialization
+	void Start () {
+        Loader.DontDestroyOnLoad(ConsoleHandler);
+        Loader.DontDestroyOnLoad(ConsoleObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(Key))
+        {
+            if (ConsoleObject.activeInHierarchy)
+            {
+                Debug.Log("Active: False");
+                ConsoleObject.SetActive(false);
+            }
+            else if (!ConsoleObject.activeInHierarchy)
+            {
+                Debug.Log("Active: True");
+                ConsoleObject.SetActive(true);
+            }
+        }
+    }
+}
